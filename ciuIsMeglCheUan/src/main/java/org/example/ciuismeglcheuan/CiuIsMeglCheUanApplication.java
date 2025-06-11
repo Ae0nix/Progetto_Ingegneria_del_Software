96ac452a8c3dca84ccc019bc227b85e5dd7590f3
Ae0nix <gianmarcolasalvia@gmail.com>
2025-06-11 15:01:19 +0200

    first commit

---
+++ ciuIsMeglCheUan/src/main/java/org/example/ciuismeglcheuan/CiuIsMeglCheUanApplication.java
@@ -0,0 +1,13 @@
+package org.example.ciuismeglcheuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiuIsMeglCheUanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiuIsMeglCheUanApplication.class, args);
+    }
+
+}
